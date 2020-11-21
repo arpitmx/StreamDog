33168f6db387f4d08d0247afb338ab184bd93f68
Kalina <kalina@Kalina.Kalina.com>
2020-11-21 11:25:30 -0500

    k

--- StreamDogJS.js
+++ StreamDogJS.js
@@ -2,11 +2,11 @@ var player = videojs("videoPlayer",{
 	autoplay: 'muted',
 	controls: true,
 	loop:true,
+	liveui :true,
 	playbackRates:[0.25,0.5,0.75,1,1.25,2,4,5],
 	userActions: {
 		hotkeys: true
 	}
 });
-
-
-console.log("player running!")
+var videoat = player.currentTime();
+console.log("Video at :",videoat)
