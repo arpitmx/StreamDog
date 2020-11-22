cc8eda3432a1b40b9ca9e301ce401f6819c35067
Kalina <kalina@Kalina.Kalina.com>
2020-11-22 12:37:08 -0500

    Progress

--- PHP.php
+++ PHP.php
@@ -4,12 +4,79 @@
 <script src="https://www.gstatic.com/firebasejs/8.1.1/firebase-auth.js"></script>
 <script src="https://www.gstatic.com/firebasejs/8.1.1/firebase-database.js"></script>
 
+<style>
 
-<!-- TODO: Add SDKs for Firebase products that you want to use
-     https://firebase.google.com/docs/web/setup#available-libraries -->
+@font-face {
+  font-family: myFirstFont;
+  src: url(sansation_light.woff);
+}
+
+
+
+
+#messagebox{
+  width: 95%;
+  height:40px;
+  left: 0px;
+  right:0px;
+ position: fixed;
+    bottom: 0px;
+    z-index: 100;
+    opacity : 50%;
+    margin-left:3px;
+    margin:5px;
+ background:WhiteSmoke;    
+
+
+}
+
+#message{
+  left:1px;
+  width:76% ;
+  height:30px;
+  
+ 
+  margin:0 auto;
+}
+
+#btn{
+  left:5px;
+  margin-left:3px;
+  width: 5em;  height: 2.5em;
+}
+#messages{
+  position: relative;
+ 
+  bottom: 10px;
+  width: 100%;
+
+ 
+
+
+  transform: rotate(180deg);
+
+list-style: square inside url("https://firebasestorage.googleapis.com/v0/b/basicly-be6a3.appspot.com/o/icons8-person-16-1.png?alt=media&token=f6a5aebc-2bc9-4953-849d-0fdbe0075b32");
+font-family: "Times New Roman", Times, serif;
+}
+ul > li {
+    transform: rotate(-180deg);
+    background:WhiteSmoke;
+    margin : 2px;
+    font-size:15px;
+    font-family: myFirstFont;
+}
+
+
+
+
+
+</style>
+
+
 
 <script>
-  // Your web app's Firebase configuration
+
+///////////////////////////////////// CONFIG ///////////////////////////////
   var firebaseConfig = {
     apiKey: "AIzaSyCIIu9E8inGtE-XQBZHRbSbw7XZJYozDt0",
     authDomain: "streamdog-b2ee9.firebaseapp.com",
@@ -19,9 +86,18 @@
     messagingSenderId: "535067991045",
     appId: "1:535067991045:web:960b781c86ec6bcc3a106c"
   };
-  // Initialize Firebase
+
   firebase.initializeApp(firebaseConfig);
 
+
+////////////////////////////////////////////////////////////////////////
+  
+  var colors = ["red","blue","cyan","black","purple","pink","brown","yellow","tomatao"];
+  
+  const random = Math.floor(Math.random() * colors.length);
+  var colorcode = colors[random];
+
+  
   var myname = prompt("Enter your name!")
 
   function sendMessage(){
@@ -40,7 +116,7 @@
   firebase.database().ref("messages").on("child_added", function(snapshot){
     var html = "";
     html += "<li>";
-      html += snapshot.val().sender + ": "+ snapshot.val().message;
+      html += "<em><b>"+snapshot.val().sender +"</b></em>"+ " : "+ snapshot.val().message;
     html += "</li";
     
     document.getElementById("messages").innerHTML += html;
@@ -50,10 +126,10 @@
 </script>
 
 
-<form id = "messagebox" onsubmit="return sendMessage();">
-<input id = "message" placeholder = "Enter message here" autocomplete="off">
-<input type = "submit">
 
+<form id = "messagebox" onsubmit="return sendMessage(); ">
+<input class = "parent" id = "message" placeholder = "Enter message here" autocomplete="off">
+<input id = "btn" class = "child btn" height =50px type = "submit" value = "Send">
 
 </form>
 
--- StreamDogJS.js
+++ StreamDogJS.js
@@ -1,13 +1,16 @@
+
+
 var player = videojs("videoPlayer",{
 	autoplay: 'muted',
 	controls: true,
 	loop:true,
+	fluid:true,
+	aspectRatio:'16:9',
 	liveui :true,
 	playbackRates:[0.25,0.5,0.75,1,1.25,2,4,5],
 	userActions: {
 		hotkeys: true
 	}
 });
-var videoat = player.currentTime();
-console.log("Video at :",videoat)
+
 
