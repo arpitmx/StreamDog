306f216b2ee37fe729a8527a169e2c01b971ddcc
arpitmaurya01 <arpitmaurya01codechef@gmail.com>
2020-11-27 14:43:50 +0530

    Updated

--- StreamDogJS.js
+++ StreamDogJS.js
@@ -1,11 +1,11 @@
 
 
 var player = videojs("videoPlayer",{
-	autoplay: 'muted',
+	autoplay: true,
 	controls: true,
 	loop:true,
-	fluid:true,
-	aspectRatio:'16:9',
+    fluid:true,
+    aspectRatio:'4:3',
 	liveui :true,
 	playbackRates:[0.25,0.5,0.75,1,1.25,2,4,5],
 	userActions: {
